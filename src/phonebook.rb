dd1f1d3cd7a91065ed962b6f37af0339c7934cf3
David Dorleus <Dorleusdavid@gmail.com>
2025-09-17 19:49:55 -0500

    Code Commit

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,41 @@
 class PhoneBook
+    require 'set'
     def initialize
+        @entries = {}
+        @listed_numbers = Set.new
+        @listed_lookup = {}
     end
 
     def add(name, number, is_listed)
-        raise Exception, "Not implemented"
+        return false if @entries.key?(name)
+        return false unless number.is_a?(String) && number.match?(/^\d{3}-\d{3}-\d{4}$/)
+        
+        if is_listed && @listed_numbers.include?(number)
+        return false
+        end
+        @entries[name] = { number: number, listed: !!is_listed }
+
+        if is_listed
+        @listed_numbers.add(number)
+        @listed_lookup[number] = name
+        end
+        true
     end
 
     def lookup(name)
-        raise Exception, "Not implemented"
+        entry = @entries[name]
+        return nil unless entry && entry[:listed]
+        entry[:number]
     end
 
     def lookupByNum(number)
-        raise Exception, "Not implemented"
+        @listed_lookup[num]
     end
 
     def namesByAc(areacode)
-        raise Exception, "Not implemented"
+        prefix = areacode + "-"
+        @entries.each_with_object([]) do |(name, entry), acc|
+        acc << name if entry[:number].start_with?(prefix)
+        end
     end
 end
